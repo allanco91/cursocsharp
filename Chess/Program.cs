0472a2f17b8c317115c0987d00defafdb6e64972
Allan Caetano de Oliveira <allan.co@hotmail.com>
2019-12-06 07:53:33 -0300

    criando a class position

---
+++ Chess/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Chess.Tabuleiro;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p;
+
+            p = new Position(3 , 4);
+
+            Console.WriteLine("Posição: " + p);
+        }
+    }
+}
