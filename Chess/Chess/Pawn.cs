f1b5c1f3d2ff1470956698a2e47dab61ebf8ca8b
Allan Caetano de Oliveira <allan.co@hotmail.com>
2019-12-06 16:10:55 -0300

    criada as classes das demais pecas

---
+++ Chess/Chess/Pawn.cs
@@ -0,0 +1,88 @@
+﻿using Chess.ChessBoard;
+
+namespace Chess.Chess
+{
+    class Pawn : Piece
+    {
+        public Pawn(Board board, Color color) : base(board, color)
+        {
+
+        }
+
+        private bool CanMove(Position pos)
+        {
+            Piece p = Board.Piece(pos);
+            return p == null || p.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            Position pos = new Position(0, 0);
+
+            //Up
+            pos.SetPosition(Position.Row - 1, Position.Column);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            //Ne
+            pos.SetPosition(Position.Row - 1, Position.Column + 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            //Right
+            pos.SetPosition(Position.Row, Position.Column + 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            //Se
+            pos.SetPosition(Position.Row + 1, Position.Column + 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            //Down
+            pos.SetPosition(Position.Row + 1, Position.Column);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            //Sw
+            pos.SetPosition(Position.Row + 1, Position.Column - 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            //Down
+            pos.SetPosition(Position.Row, Position.Column - 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            //Nw
+            pos.SetPosition(Position.Row - 1, Position.Column - 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            return mat;
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+    }
+}
